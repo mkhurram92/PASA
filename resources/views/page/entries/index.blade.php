24fc93bd806176563dc11b11b383f0ba0a810266
Mirza <mkhurram92@gmail.com>
2024-02-22 16:24:48 +1030

    Fix Add Entry Button on Index Page

--- resources/views/page/entries/index.blade.php
+++ resources/views/page/entries/index.blade.php
@@ -22,44 +22,43 @@
                      <h3 class="page-title">Entries List</h3>
                  </div>
                  <div class="card-header d-flex justify-content-between align-items-center">
-                     <a class="btn btn-primary" href="javascript:void(0)" id="create-record">
-                         <i class="fa fa-plus-circle" style="font-size:24px;"></i>
-                     </a>
-                 </div>
+                    <a class="btn btn-primary" href="{{ route('entries.create') }}">
+                        <i class="fa fa-plus-circle" style="font-size:24px;">Add Entry</i>
+                    </a>
+                </div>
              </div>
-             <a href="{{ route('entries.create') }}" class="btn btn-success">Create Entry</a>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Account</th>
-                <th>Transaction</th>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($entries as $entry)
-                <tr>
-                    <td>{{ $entry->id }}</td>
-                    <td>{{ $entry->account->name }}</td>
-                    <td>{{ $entry->transaction->description }}</td>
-                    <td>{{ $entry->type }}</td>
-                    <td>{{ $entry->amount }}</td>
-                    <td>
-                        <a href="{{ route('entries.show', $entry->id) }}" class="btn btn-info">View</a>
-                        <a href="{{ route('entries.edit', $entry->id) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('entries.destroy', $entry->id) }}" method="POST" style="display: inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+             <table class="table">
+                 <thead>
+                     <tr>
+                         <th>ID</th>
+                         <th>Account</th>
+                         <th>Transaction</th>
+                         <th>Type</th>
+                         <th>Amount</th>
+                         <th>Actions</th>
+                     </tr>
+                 </thead>
+                 <tbody>
+                     @foreach ($entries as $entry)
+                     <tr>
+                         <td>{{ $entry->id }}</td>
+                         <td>{{ $entry->account->name }}</td>
+                         <td>{{ $entry->transaction->description }}</td>
+                         <td>{{ $entry->type }}</td>
+                         <td>{{ $entry->amount }}</td>
+                         <td>
+                             <a href="{{ route('entries.show', $entry->id) }}" class="btn btn-info">View</a>
+                             <a href="{{ route('entries.edit', $entry->id) }}" class="btn btn-warning">Edit</a>
+                             <form action="{{ route('entries.destroy', $entry->id) }}" method="POST" style="display: inline;">
+                                 @csrf
+                                 @method('DELETE')
+                                 <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                             </form>
+                         </td>
+                     </tr>
+                     @endforeach
+                 </tbody>
+             </table>
 
-@include('layout.footer')
+             @include('layout.footer')
