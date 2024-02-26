485be12901c6c44f77fd4582e779c0a768d8b105
Khurram Shahzad Mirza <53279133+mkhurram92@users.noreply.github.com>
2024-02-26 23:30:54 +1030

    Member Wizard Father | Mother Data Save

--- resources/views/page/member-form-wizard/account.blade.php
+++ resources/views/page/member-form-wizard/account.blade.php
@@ -5,28 +5,17 @@
         <div class="col-md-4 my-2">
             <label class="form-control-label">Username <span class="tx-danger">*</span></label>
             <input class="form-control" id="username" name="username" value="{{ old('username') }}"
-                placeholder="Enter username" required="" type="text">
+                 required="" type="text">
         </div>
-        <!--<div class="col-md-4 my-2">
-            <label class="form-control-label">Password <span class="tx-danger">*</span></label>
-            <input class="form-control" id="password" name="password" value="{{ old('password') }}"
-                placeholder="Enter password" required="" type="password">
-        </div>
-        <div class="col-md-4 my-2">
-            <label class="form-control-label">Confirmation Password <span class="tx-danger">*</span></label>
-            <input class="form-control" id="password_confirmation" name="password_confirmation"
-                value="{{ old('password_confirmation') }}" placeholder="Enter same password" required=""
-                type="password">
-        </div>-->
         <div class="col-md-4 my-2">
             <label class="form-control-label">Email <span class="tx-danger">*</span></label>
             <input class="form-control" id="email" name="email" value="{{ old('email') }}"
-                placeholder="Enter Email" required="" type="text">
+                 required="" type="text">
         </div>
         <div class="col-md-4 my-2">
             <label class="form-control-label">Confirmation Email <span class="tx-danger">*</span></label>
             <input class="form-control" id="email_confirmation" name="email_confirmation"
-                value="{{ old('email_confirmation') }}" placeholder="Enter Same Email" required="" type="email">
+                value="{{ old('email_confirmation') }}"  required="" type="email">
         </div>
     </div>
     <div class="row">
@@ -57,36 +46,36 @@
                 <div class="col-md-4 my-2">
                     <label class="form-control-label">Given name <span class="tx-danger">*</span></label>
                     <input class="form-control" id="given_name" name="given_name" value="{{ old('given_name') }}"
-                        placeholder="Enter given name" required="" type="text">
+                         required="" type="text">
                 </div>
                 <div class="col-md-4 my-2">
                     <label class="form-control-label">Family Name <span class="tx-danger">*</span></label>
                     <input class="form-control" id="family_name" name="family_name" value="{{ old('family_name') }}"
-                        placeholder="Enter Family name" required="" type="text">
+                         required="" type="text">
                 </div>
                 <div class="col-md-6 my-2">
                     <label class="form-control-label">Preferred Name <span class="tx-danger">*</span></label>
                     <input class="form-control" id="preferred_name" name="preferred_name"
-                        value="{{ old('preferred_name') }}" placeholder="Enter Preferred Name" required=""
+                        value="{{ old('preferred_name') }}"  required=""
                         type="text">
                 </div>
                 <div class="col-md-6 my-2">
                     <label class="form-control-label">Birth Date <span class="tx-danger">*</span></label>
                     <input class="form-control" id="date_of_birth" name="date_of_birth"
-                        value="{{ old('date_of_birth') }}" placeholder="Enter Date of Birth" required=""
+                        value="{{ old('date_of_birth') }}"  required=""
                         type="date">
                 </div>
 
                 <div class="col-md-12 my-2">
                     <label class="form-control-label">Unit No / Number & Street <span class="tx-danger">*</span></label>
                     <input class="form-control" id="number_street" name="number_street"
-                        value="{{ old('number_street') }}" placeholder="Enter Number & Street" required=""
+                        value="{{ old('number_street') }}"  required=""
                         type="text">
                 </div>
                 <div class="col-md-6 my-2">
                     <label class="form-control-label">Suburb <span class="tx-danger">*</span></label>
                     <input class="form-control" id="suburb" name="suburb" value="{{ old('suburb') }}"
-                        placeholder="Enter Suburb" required="" type="text">
+                         required="" type="text">
                 </div>
                 <div class="col-md-6 my-2">
                     <label class="form-control-label">State <span class="tx-danger">*</span></label>
@@ -107,17 +96,17 @@
                 <div class="col-md-6 my-2">
                     <label class="form-control-label">Post Code/Zip <span class="tx-danger">*</span></label>
                     <input class="form-control" id="post_code" name="post_code" value="{{ old('post_code') }}"
-                        placeholder="Enter Post Code/Zip" required="" type="text">
+                         required="" type="text">
                 </div>
                 <div class="col-md-6 my-2">
                     <label class="form-control-label">Phone (Home) </label>
                     <input class="form-control" id="phone" name="phone" value="{{ old('phone') }}"
-                        placeholder="Enter Phone (Home)" required="" type="text">
+                         required="" type="text">
                 </div>
                 <div class="col-md-6 my-2">
                     <label class="form-control-label">Phone (Mobile) </label>
                     <input class="form-control" id="mobile" name="mobile" value="{{ old('mobile') }}"
-                        placeholder="Enter Phone (Mobile)" required="" type="text">
+                         required="" type="text">
                 </div>
             </div>
         </div>
