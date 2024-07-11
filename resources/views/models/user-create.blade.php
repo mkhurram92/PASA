05da57a4be8ea83ccef5c30e38762b65ba918a86
Khurram Shahzad Mirza <53279133+mkhurram92@users.noreply.github.com>
2024-07-11 23:25:28 +0930

    Role Layout Update

--- resources/views/models/user-create.blade.php
+++ resources/views/models/user-create.blade.php
@@ -10,44 +10,40 @@
             <div class="modal-body">
                 <form id="userForm" name="userForm" class="form-horizontal" method="POST" action="{{ route('user.store') }}">
                     @csrf
-                    <div class="form-group">
-                        <div class="col-sm-12">
-                            <label for="name" class="control-label">Name</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="" maxlength="50" required="">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="name" class="control-label">Name</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="" maxlength="50" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="role_id" class="control-label">Role</label>
+                                <select class="form-control" id="role_id" name="role_id" required>
+                                    <option value="">Select Role</option>
+                                    @foreach($roles as $role)
+                                        <option value="{{ $role->id }}">{{ $role->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="email" class="control-label">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" value="" maxlength="50" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="control-label">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password" value="" maxlength="50" required="">
+                            </div>
                         </div>
                     </div>
 
-                    <div class="form-group">
-                        <div class="col-sm-12">
-                            <label for="email" class="control-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" value="" maxlength="50" required="">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <button type="submit" class="btn btn-primary btn-block" id="saveBtn" value="create">Save changes</button>
                         </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-sm-12">
-                            <label for="password" class="control-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password" value="" maxlength="50" required="">
-                        </div>
-                    </div>
-
-                    <!-- Role selection field -->
-                    <div class="form-group">
-                        <div class="col-sm-12">
-                            <label for="role_id" class="control-label">Role</label>
-                            <select class="form-control" id="role_id" name="role_id" required>
-                                <option value="">Select Role</option>
-                                @foreach($roles as $role)
-                                    <option value="{{ $role->id }}">{{ $role->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-sm-12">
-                            <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Save changes</button>
-                            <button class="btn btn-light" data-bs-dismiss="modal" type="button">Close</button>
+                        <div class="col-md-6">
+                            <button class="btn btn-light btn-block" data-bs-dismiss="modal" type="button">Close</button>
                         </div>
                     </div>
                 </form>
