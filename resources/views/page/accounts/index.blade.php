01e60c8766ea2f9eef5ad5069f0a3d35891e9668
Mirza <mkhurram92@gmail.com>
2024-09-30 10:29:10 +0930

    Hide Date Range

--- resources/views/page/accounts/index.blade.php
+++ resources/views/page/accounts/index.blade.php
@@ -116,8 +116,7 @@
                                      <option value="100">100</option>
                                      <option value="1000000">ALL</option>
                                  </select>
-                                 <label style="padding: 10px;" for="date-range">Date Range:</label>
-                                 <input style="padding: 10px 20px;" type="text" id="date-range">
+                                 
                                  <button class="custom-button" type="button" id="printTable"
                                      onclick="printData()">Print</button>
                                  <button class="custom-button" id="download-csv">Download CSV</button>
